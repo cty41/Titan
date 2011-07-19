034ab6899e7f4922251d69caeaf1a6de1b189f64
cty41 <ctyFranky@gmail.com>
2011-07-19 23:18:50 +0800

    update maya exporter proj

---
+++ MayaExporter/MayaHeader.h
@@ -0,0 +1,39 @@
+#ifndef TITAN_MAYA_HEADER__HH
+#define TITAN_MAYA_HEADER__HH
+
+
+#include <iostream>
+#include <string.h> 
+#include <sys/types.h>
+#include <maya/MStatus.h>
+#include <maya/MPxCommand.h>
+#include <maya/MString.h>
+#include <maya/MStringArray.h>
+#include <maya/MArgList.h>
+#include <maya/MGlobal.h>
+#include <maya/MSelectionList.h>
+#include <maya/MItSelectionList.h>
+#include <maya/MPoint.h>
+#include <maya/MPointArray.h>
+#include <maya/MDagPath.h>
+#include <maya/MDagPathArray.h>
+#include <maya/MFnPlugin.h>
+#include <maya/MFnMesh.h>
+#include <maya/MFnSet.h>
+#include <maya/MItMeshPolygon.h>
+#include <maya/MItMeshVertex.h>
+#include <maya/MItMeshEdge.h>
+#include <maya/MFloatVector.h>
+#include <maya/MFloatVectorArray.h>
+#include <maya/MFloatArray.h>
+#include <maya/MObjectArray.h>
+#include <maya/MObject.h>
+#include <maya/MPlug.h>
+#include <maya/MPxFileTranslator.h>
+#include <maya/MFnDagNode.h>
+#include <maya/MItDag.h>
+#include <maya/MDistance.h>
+#include <maya/MIntArray.h>
+#include <maya/MIOStream.h>
+
+#endif
